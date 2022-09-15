fed7e27c7b8bab85c747976d377d1841de76e74f
Nilton <niltonroese@gmail.com>
2022-09-15 19:46:34 +1000

    all passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,21 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if !params[:name] || params[:name].empty?
-            redirect_to controller: 'sessions', action: 'new'
-        else
-            session[:name] = params[:name]
-            redirect_to root_path
-        end
+        return redirect_to controller: 'sessions', action: 'new' if !params[:name] || params[:name].empty?
+        session[:name] = params[:name]
+        redirect_to controller: 'application', action: 'hello'
+        # redirect_to controller: 'sessions', action: 'new'
+        # if !params[:name] || params[:name].empty?
+        #     redirect_to controller: 'sessions', action: 'new'
+        # else
+        #     session[:name] = params[:name]
+        #     redirect_to root_path
+        # end
     end
 
     def destroy
         session.delete :name
-        redirect_to login_path
+        redirect_to controller: 'sessions', action: 'new'
     end
 
 end
